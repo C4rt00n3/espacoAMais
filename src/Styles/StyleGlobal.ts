00a4d1d290d9b3547443cd230aeae22ee6a245d9
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-01-12 19:10:05 -0300

    Initial commit

--- src/Styles/StyleGlobal.ts
+++ src/Styles/StyleGlobal.ts
@@ -18,6 +18,17 @@ export const StyleGlobal = createGlobalStyle`
 
         --Grey100: #423F44;
         --Grey50: #2B2A2A;
-        
+
+
+        --Font12: 0.75rem;
+        --Font14: 0.875rem;
+        --Font16: 1rem;
+        --Font18: 1.125rem;
+        --Font20: 1.25rem;
+        --Font24: 1.5rem;
+        --Font32: 2rem;
+        --Font12: 2.25rem;
+
+
     }
 `;
